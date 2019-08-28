dc20563de3d1f0bda28de828c1857aeba3719088
Filtrar por fecha <>
2019-08-28 10:04:36 -0500

    Estructuración del proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 5.5 | Componentes Vuejs</title>
+
+        {{-- Estilos --}}
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- Fin Estilos --}}
+    </head>
+    <body>
+        
+    {{-- Scripts --}}
+        <script src="{{ asset('js/app.js') }}"></script>
+    {{-- FinScripts --}}
+    </body>
+</html>
