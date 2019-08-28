410e158373e8ee1a46a28d03b0e864d725be71eb
Filtrar por fecha <>
2019-08-28 17:24:45 -0500

    Creando el componente Idea

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,31 +19,10 @@
 
                         {{-- ID para usar vue --}}
                         <div id="app">
-                            <h2 class="text-center">Captura tus ideas</h2>
-                            <div class="well">
-                                <h4>¿En qué estás pensando?</h4>
-                                <form action="">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control input-sm" maxlength="256">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-primary btn-sm">
-                                                Agregar
-                                            </button>
-                                        </span>
-                                    </div>
-                                </form>
-                                <hr>
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <p>
-                                            <small class="text-muted">
-                                                <em>hace un minuto</em>
-                                            </small>
-                                            Mi nueva idea
-                                        </p>
-                                    </li>
-                                </ul>
-                            </div>
+
+                            {{-- Componente Idea --}}
+                                <idea></idea>
+
                         </div>
 
                     </div>
