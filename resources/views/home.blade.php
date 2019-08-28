c3a28a65eecabe5f7864270362e8ca4534d25df3
Filtrar por fecha <>
2019-08-28 11:15:17 -0500

    Diseño vista Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,44 @@
         {{-- Fin Estilos --}}
     </head>
     <body>
-        
+        {{-- Contenido --}}
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-8 col-xs-offset-2">
+
+                        {{-- ID para usar vue --}}
+                        <div id="app">
+                            <h2 class="text-center">Captura tus ideas</h2>
+                            <div class="well">
+                                <h4>¿En qué estás pensando?</h4>
+                                <form action="">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control input-sm" maxlength="256">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-primary btn-sm">
+                                                Agregar
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                                <hr>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <p>
+                                            <small class="text-muted">
+                                                <em>hace un minuto</em>
+                                            </small>
+                                            Mi nueva idea
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        {{-- Fin Contenido --}}
     {{-- Scripts --}}
         <script src="{{ asset('js/app.js') }}"></script>
     {{-- FinScripts --}}
