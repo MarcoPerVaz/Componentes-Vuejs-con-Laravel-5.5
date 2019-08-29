03f9d87da68ef277b2efdcdb1016920403b5dd51
Filtrar por fecha <>
2019-08-29 01:07:40 -0500

    Implementando Momentjs y toastr

--- resources/assets/js/components/Idea.vue
+++ resources/assets/js/components/Idea.vue
@@ -18,7 +18,7 @@
                 <li v-for="idea in ideas" :key="idea.description">
                     <p>
                         <small class="text-muted">
-                            <em>{{ idea.created_at }}</em>
+                            <em>{{ since(idea.created_at) }}</em>
                         </small>
                         {{ idea.description }}
                     </p>
@@ -29,8 +29,11 @@
 </template>
 
 <script>
-    // Por si instalas la librería sin usar npm, acá lo debes importar si solo se va a usar en este componente
+    // Por si instalas la librería sin usar npm, acá lo debes importar si solo se va a usar en este componente, 
+        // si se requieren en otros componentes deben importarse desde resources/assets/js/app.js
     import axios from 'axios'
+    import toastr from 'toastr'
+    import moment from 'moment'
 
     export default {
         data() {
@@ -44,6 +47,11 @@
         },
 
         methods: {
+            // Función para tranformar la variable al formato con Momentjs
+            since: function(date)
+            {
+                return moment(date).fromNow();
+            },
             getIdeas:function() {
                 var urlIdeas = "mis-ideas";
                 axios.get(urlIdeas).then(response => {
