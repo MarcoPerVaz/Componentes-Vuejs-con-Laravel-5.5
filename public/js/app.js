80fdbaccefa684a514a283dd3b5c90c01259f391
Filtrar por fecha <>
2019-08-29 13:29:08 -0500

    Guardar tareas, timezone y diseño de Toastr

--- public/js/app.js
+++ public/js/app.js
@@ -61634,10 +61634,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
+__WEBPACK_IMPORTED_MODULE_2_moment___default.a.lang('es'); /* Para cambiar el lenguaje de Momentjs */
+
 /* harmony default export */ __webpack_exports__["default"] = ({
     data: function data() {
         return {
-            ideas: []
+            ideas: [],
+            newIdea: ''
         };
     },
 
@@ -61657,6 +61660,22 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             var urlIdeas = "mis-ideas";
             __WEBPACK_IMPORTED_MODULE_0_axios___default.a.get(urlIdeas).then(function (response) {
                 _this.ideas = response.data;
+            });
+        },
+        createIdea: function createIdea() {
+            var _this2 = this;
+
+            var url = 'guardar-idea';
+            __WEBPACK_IMPORTED_MODULE_0_axios___default.a.post(url, {
+                description: this.newIdea
+            }).then(function (response) {
+                _this2.getIdeas(); /* Se obtienen todas las ideas */
+                _this2.newIdea = ''; /* Se limpia la caja de texto newIdea */
+                // Toastr
+                __WEBPACK_IMPORTED_MODULE_1_toastr___default.a.success('Nueva idea registrada');
+            }).catch(function (error) {
+                // Toastr
+                __WEBPACK_IMPORTED_MODULE_1_toastr___default.a.error('Error');
             });
         }
     }
@@ -62438,7 +62457,44 @@ var render = function() {
     _c("div", { staticClass: "well" }, [
       _c("h4", [_vm._v("¿En qué estás pensando?")]),
       _vm._v(" "),
-      _vm._m(0),
+      _c(
+        "form",
+        {
+          on: {
+            submit: function($event) {
+              $event.preventDefault()
+              return _vm.createIdea($event)
+            }
+          }
+        },
+        [
+          _c("div", { staticClass: "input-group" }, [
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.newIdea,
+                  expression: "newIdea"
+                }
+              ],
+              staticClass: "form-control input-sm",
+              attrs: { type: "text", maxlength: "256" },
+              domProps: { value: _vm.newIdea },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.newIdea = $event.target.value
+                }
+              }
+            }),
+            _vm._v(" "),
+            _vm._m(0)
+          ])
+        ]
+      ),
       _vm._v(" "),
       _c("hr"),
       _vm._v(" "),
@@ -62469,18 +62525,9 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("form", { attrs: { action: "" } }, [
-      _c("div", { staticClass: "input-group" }, [
-        _c("input", {
-          staticClass: "form-control input-sm",
-          attrs: { type: "text", maxlength: "256" }
-        }),
-        _vm._v(" "),
-        _c("span", { staticClass: "input-group-btn" }, [
-          _c("button", { staticClass: "btn btn-primary btn-sm" }, [
-            _vm._v("\n                        Agregar\n                    ")
-          ])
-        ])
+    return _c("span", { staticClass: "input-group-btn" }, [
+      _c("button", { staticClass: "btn btn-primary btn-sm" }, [
+        _vm._v("\n                        Agregar\n                    ")
       ])
     ])
   }
--- resources/assets/js/components/Idea.vue
+++ resources/assets/js/components/Idea.vue
@@ -3,9 +3,9 @@
         <h2 class="text-center">Captura tus ideas</h2>
         <div class="well">
             <h4>¿En qué estás pensando?</h4>
-            <form action="">
+            <form v-on:submit.prevent="createIdea">
                 <div class="input-group">
-                    <input type="text" class="form-control input-sm" maxlength="256">
+                    <input type="text" class="form-control input-sm" maxlength="256" v-model="newIdea">
                     <span class="input-group-btn">
                         <button class="btn btn-primary btn-sm">
                             Agregar
@@ -33,12 +33,15 @@
         // si se requieren en otros componentes deben importarse desde resources/assets/js/app.js
     import axios from 'axios'
     import toastr from 'toastr'
-    import moment from 'moment'
+    import moment from 'moment' /* No olvidar importar los estilos en sass/app.scss */
+
+    moment.lang('es'); /* Para cambiar el lenguaje de Momentjs */
 
     export default {
         data() {
             return {
                 ideas: [],
+                newIdea: ''
             }
         },
 
@@ -52,11 +55,27 @@
             {
                 return moment(date).fromNow();
             },
-            getIdeas:function() {
+            getIdeas:function() 
+            {
                 var urlIdeas = "mis-ideas";
                 axios.get(urlIdeas).then(response => {
                     this.ideas = response.data;
                 })
+            },
+            createIdea: function()
+            {
+                var url = 'guardar-idea';
+                axios.post(url, {
+                    description: this.newIdea
+                }).then(response => {
+                    this.getIdeas(); /* Se obtienen todas las ideas */
+                    this.newIdea = ''; /* Se limpia la caja de texto newIdea */
+                    // Toastr
+                    toastr.success('Nueva idea registrada');
+                }).catch(error => {
+                    // Toastr
+                    toastr.error('Error');
+                });
             }
         }
     }
